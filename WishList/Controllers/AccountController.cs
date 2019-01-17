0450befb3222d950e1ec62b90573f7a9678ed9c9
Pavel Yermalovich <pavel.yermalovich@dotmailer.com>
2019-01-17 14:24:51 +0300

    Added Authorize attribute

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
 
 namespace WishList.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
