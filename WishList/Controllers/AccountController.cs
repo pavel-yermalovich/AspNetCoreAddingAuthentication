095ae0e468831a2c10a28a7f02f64d34e855a893
Pavel Yermalovich <pavel.yermalovich@dotmailer.com>
2019-01-17 14:00:04 +0300

    Created AccountController

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
